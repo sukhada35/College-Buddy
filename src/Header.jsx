ce51e0b8a13413a2c63c7309cf98a53c3a8fa636
Ashmit <ashmit27j@gmail.com>
2025-02-12 22:13:05 +0530

    refactor: remove unused CSS and restructure HTML for improved component organization

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+	return (
+		<header>
+			<h1>College Buddy website</h1>
+			<nav>
+				<ul>
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+					<li>
+						<a href="/contact">Contact</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
+export default Header;
